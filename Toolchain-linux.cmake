fafe059208f76747cc4f9b73405a5021c30e7693
d.bouron <d.bouron@criteo.com>
2019-10-01 15:33:43 +0200

    Change configuration build order
    When Windows target is built first, the Linus one fails.
    The output error claims that /bin/cc is broken.
    The workaround is to build Linux first and then Windows 64.

--- Toolchain-linux.cmake
+++ Toolchain-linux.cmake
@@ -6,7 +6,6 @@ SET(CMAKE_SYSTEM_NAME Linux)
 # which compilers to use for C and C++
 SET(CMAKE_C_COMPILER gcc)
 SET(CMAKE_CXX_COMPILER g++)
-SET(CMAKE_RC_COMPILER windres)
 SET(CMAKE_AR ar)
 SET(CMAKE_RANLIB ranlib)
 
