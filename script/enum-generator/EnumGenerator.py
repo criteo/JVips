976e4a4d70bae0d0919682353ac2f90421b7be4c
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-10 06:15:52 +0000

    Adding tarfile member sanitization to extractall()

--- script/enum-generator/EnumGenerator.py
+++ script/enum-generator/EnumGenerator.py
@@ -247,7 +247,26 @@ def main():
         url = f'https://github.com/libvips/libvips/releases/download/v{version}/vips-{version}.tar.gz'
         tarball = wget.download(url)
         with tarfile.open(tarball) as tf:
-            tf.extractall()
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tf)
 
     with open(os.path.join(os.getcwd(), 'LICENSE'), 'r', encoding='utf-8') as infile:
         license_comment = infile.read()
